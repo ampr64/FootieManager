bd8991de3b555d70b2d26463534473099c0210b3
Álvaro Prieto <alviprieto@hotmail.com>
2020-10-01 14:06:28 -0300

    Implement IRepository

--- src/Infrastructure/Data/Repository.cs
+++ src/Infrastructure/Data/Repository.cs
@@ -1,73 +1,83 @@
-﻿using Domain.Common;
-using Domain.Data;
+﻿using Core.Common;
+using Infrastructure.Extensions;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Data
 {
     public class Repository<TEntity, TId> : IRepository<TEntity, TId>
         where TEntity : Entity<TId>
+        where TId : IEquatable<TId>
     {
-        protected readonly DbContext _dbContext;
+        protected readonly FootieManagerContext _dbContext;
 
-        public Repository(DbContext dbContext)
+        public Repository(FootieManagerContext context)
         {
-            _dbContext = dbContext;
+            _dbContext = context;
         }
 
-        public Task<TEntity> AddAsync(TEntity entity)
+        public async Task<TEntity> AddAsync(TEntity entity)
         {
-            throw new NotImplementedException();
+            await _dbContext.AddAsync(entity);
+            return entity;
         }
 
-        public Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<bool> AllAsync(Expression<Func<TEntity, bool>> predicate) => await _dbContext.Set<TEntity>().AllAsync(predicate);
 
-        public Task<TEntity> FindAsync(TId id, params Expression<Func<TEntity, object>>[] includeProperties)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate) => await _dbContext.Set<TEntity>().AnyAsync(predicate);
 
-        public Task<TEntity> FirstAsync(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includeProperties)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<bool> ContainsAsync(TEntity entity) => await _dbContext.Set<TEntity>().ContainsAsync(entity);
+
+        public async Task<int> CountAsync(Expression<Func<TEntity, bool>> predicate = null) => await _dbContext.Set<TEntity>().CountAsync(predicate);
+
+        public async Task<TEntity> FindAsync(TId id, params Expression<Func<TEntity, object>>[] includeProperties) =>
+            await _dbContext.Set<TEntity>()
+                .IncludeMultiple(includeProperties)
+                .SingleOrDefaultAsync(entity => entity.Id.Equals(id));
 
         public Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includeProperties)
         {
             throw new NotImplementedException();
         }
 
-        public Task<IReadOnlyList<TEntity>> ListAllAsync(string includeProperties = null)
+        public Task ForEachAsync(Action<TEntity> action)
         {
             throw new NotImplementedException();
         }
 
-        public Task<IReadOnlyList<TEntity>> ListAsync(Expression<Func<TEntity, bool>> predicate, string includeProperties)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<IReadOnlyList<TEntity>> ListAllAsync(string includeProperties = null) =>
+            await _dbContext.Set<TEntity>()
+                            .Include(includeProperties)
+                            .ToListAsync();
+
+        public async Task<IReadOnlyList<TEntity>> ListAsync(Expression<Func<TEntity, bool>> predicate, string includeProperties) =>
+            await _dbContext.Set<TEntity>()
+                            .Include(includeProperties)
+                            .Where(predicate)
+                            .ToListAsync();
 
         public Task RemoveAsync(TEntity entity)
         {
-            throw new NotImplementedException();
+            _dbContext.Remove(entity);
+            return Task.CompletedTask;
         }
 
-        public Task RemoveAsync(TId id)
+        public async Task RemoveAsync(TId id)
         {
-            throw new NotImplementedException();
+            var entity = await _dbContext.FindAsync<TEntity>(id);
+            _dbContext.Remove(entity);
         }
+
+        public async Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includeProperties) => await _dbContext.Set<TEntity>().SingleOrDefaultAsync(predicate);
 
         public Task UpdateAsync(TEntity entity)
         {
-            throw new NotImplementedException();
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            return Task.CompletedTask;
         }
     }
 }
