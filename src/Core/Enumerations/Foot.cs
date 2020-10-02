cd3b7d695a4d3e250b788a66fdebe29168850246
Álvaro Prieto <alviprieto@hotmail.com>
2020-10-02 13:25:15 -0300

    Add foot and goal type enumerations.

---
+++ src/Core/Enumerations/Foot.cs
@@ -0,0 +1,14 @@
+﻿using Core.Common;
+
+namespace Core.Enumerations
+{
+    public class Foot : Enumeration
+    {
+        public static Enumeration Right = new Foot(1, nameof(Right));
+        public static Enumeration Left = new Foot(2, nameof(Left));
+
+        private Foot(int value, string name) : base(value, name)
+        {
+        }
+    }
+}
