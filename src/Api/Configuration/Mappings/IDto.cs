ab57f90d9b5f46f29158cc1ced9ceddd8318bbf1
Álvaro Prieto <alviprieto@hotmail.com>
2020-11-24 01:44:12 -0300

    Add mapping configuration.

---
+++ src/Api/Configuration/Mappings/IDto.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace Api.Configuration.Mappings
+{
+    public interface IDto<T>
+    {
+        void Map(Profile profile) => profile.CreateMap(typeof(T), GetType());
+    }
+}
---
+++ src/Api/Extensions/MappingExtensions.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Api.Extensions
+{
+    public static class MappingExtensions
+    {
+        public static async Task<List<TDestination>> ProjectToListAsync<TDestination>(
+            this IQueryable source,
+            IConfigurationProvider configuration,
+            CancellationToken cancellationToken = default)
+        {
+            return await source.ProjectTo<TDestination>(configuration).ToListAsync(cancellationToken);
+        }
+    }
+}
